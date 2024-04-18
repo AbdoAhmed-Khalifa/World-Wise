8309a505f6471870f4263d8648b01a386f89531e
Abdelrahman Khalifa <abdelrahmankhalifa20@yahoo.com>
2024-04-18 15:07:09 +0200

    update

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -10,7 +10,7 @@ function CityList() {
   if (isLoading) return <Spinner />;
   if (!cities.length)
     return (
-      <Message message="Add your first city by clicking on a city on the map" />
+      <Message message="Add your first city by clicking on a city on the Map" />
     );
   return (
     <ul className={styles.cityList}>
